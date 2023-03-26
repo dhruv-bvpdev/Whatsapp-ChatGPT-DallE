01c38966b03bdc7f695df41feda7623cc22bf277
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2023-03-26 16:35:09 +0530

    fix: :closed_lock_with_key: fix env (PREFIX_ENABLED)

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ const config: IConfig = {
   openAIAPIKey: process.env.OPENAI_API_KEY || '',
 
   //* Prefix
-  prefixEnabled: process.env.PREFIX_ENABLED == 'true',
+  prefixEnabled: getEnvBooleanWithDefault('PREFIX_ENABLED', true), // Default: true
   gptPrefix: process.env.GPT_PREFIX || '!gpt', // Default: !gpt
   dallePrefix: process.env.DALLE_PREFIX || '!dalle', // Default: !dalle
   aiConfigPrefix: process.env.AI_CONFIG_PREFIX || '!config', // Default: !config
@@ -33,6 +33,13 @@ const config: IConfig = {
   //* Transcription & Text-to-Speech
   ttsEnabled: process.env.TTS_ENABLED == 'true' || false, // Default: false
   transcriptionEnabled: process.env.TRANSCRIPTION_ENABLED == 'true' || false // Default: false
+}
+
+function getEnvBooleanWithDefault(key: string, defaultValue: boolean): boolean {
+  if (process.env[key] == undefined || process.env[key] == '') {
+    return defaultValue
+  }
+  return process.env[key] == 'true'
 }
 
 export default config
